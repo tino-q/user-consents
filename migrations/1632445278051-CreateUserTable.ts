9be77b20ea9b950922526ae597490ec6637367b2
martinqueija <tinqueija@gmail.com>
2021-09-26 18:45:21 +0200

    fix: enable uuids in migration

--- migrations/1632445278051-CreateUserTable.ts
+++ migrations/1632445278051-CreateUserTable.ts
@@ -1,8 +1,9 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateUserTable1632388241609 implements MigrationInterface {
+export class CreateUserTable1632445278051 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
+      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
       CREATE TABLE "User" (
         "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
         "email" text NOT NULL,
